004aa148714ed21308431b32219ec1bdf8e27feb
ChanningTong <Channing_TongCN@outlook.com>
2024-05-31 11:32:39 +0800

    Initial Commit

---
+++ GV_Core_with_OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GV_Core_with_OpenGL
+//
+//  Created by ChanningTong on 5/31/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
